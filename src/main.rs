d2135da511644083e91ff518a0d93f39d9c8c2a2
AugustynM <augustyn.majtyka@student.uj.edu.pl>
2023-12-11 11:22:53 +0100

    specified number of lines

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,21 @@ use clap::{command, Arg, Command};
 
 fn main() {
     let matches = command!() // requires `cargo` feature
-        .subcommand(Command::new("head").arg(Arg::new("path")))
-        .subcommand(Command::new("tail").arg(Arg::new("path")))
+        .subcommand(Command::new("head").arg(Arg::new("path")).arg(Arg::new("n")))
+        .subcommand(Command::new("tail").arg(Arg::new("path")).arg(Arg::new("n")))
         .get_matches();
 
     match matches.subcommand() {
         Some((name, args)) => match name {
             "head" => {
                 let path = Path::new(args.get_one::<String>("path").unwrap());
-                println!("{:?}", path);
+                let n = args.get_one::<String>("n").unwrap().parse::<usize>().unwrap();
+                println!("{:?}, {n}", path);
             }
             "tail" => {
                 let path = Path::new(args.get_one::<String>("path").unwrap());
-                println!("{:?}", path);
+                let n = args.get_one::<String>("n").unwrap().parse::<usize>().unwrap();
+                println!("{:?}, {n}", path);
             }
             _ => panic!(),
         },
