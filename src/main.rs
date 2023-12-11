f2d6e25331d6bd6e5019b668c6ed46067df52fe9
AugustynM <augustyn.majtyka@student.uj.edu.pl>
2023-12-11 10:57:11 +0100

    basic subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
+use std::path::Path;
+
+use clap::{command, Arg, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = command!() // requires `cargo` feature
+        .subcommand(Command::new("head").arg(Arg::new("path")))
+        .subcommand(Command::new("tail").arg(Arg::new("path")))
+        .get_matches();
+
+    match matches.subcommand() {
+        Some((name, args)) => match name {
+            "head" => {
+                let path = Path::new(args.get_one::<String>("path").unwrap());
+                println!("{:?}", path);
+            }
+            "tail" => {
+                let path = Path::new(args.get_one::<String>("path").unwrap());
+                println!("{:?}", path);
+            }
+            _ => panic!(),
+        },
+        None => panic!(),
+    }
 }
 
 #[cfg(test)]
